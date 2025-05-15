39612f13d616bfea6ccf01d20800e0a22fd4f321
Hashir Asad <145664273+WeWeBunnyX@users.noreply.github.com>
2025-05-15 22:52:40 +0500

    Add Colors for Post Outputs : Title, Author, Body

--- get_posts.sh
+++ get_posts.sh
@@ -9,14 +9,20 @@ if [[ -z "$subreddit" ]]; then
 fi
 
 if [[ -z "$limit" ]]; then
-    limit=100
+    limit=10  
 fi
 
 access_token=$(cat access_token.txt)
 
+# Color VARs
+TITLE_COLOR='\e[96m'
+AUTHOR_COLOR='\e[93m'
+BODY_COLOR='\e[92m'
+RESET='\e[0m'
+
 curl -s -H "Authorization: Bearer $access_token" \
      -H "User-Agent: bash:termuddit:v1.0 (by /u/WeWeBunnyX)" \
-     "https://oauth.reddit.com/r/$subreddit/hot?limit=$limit" | jq -r '
+     "https://oauth.reddit.com/r/$subreddit/hot?limit=$limit" | jq -r --arg TITLE_COLOR "$TITLE_COLOR" --arg AUTHOR_COLOR "$AUTHOR_COLOR" --arg BODY_COLOR "$BODY_COLOR" --arg RESET "$RESET" '
 .data.children[] |
-  "🔸 \(.data.title)\n🌐 \(.data.url)\n👤 Author: \(.data.author)\n📝 \(.data.selftext)\n─────────────────────────────"
+  "\($TITLE_COLOR)🔸 \(.data.title)\($RESET)\n\($AUTHOR_COLOR)👤 Author: \(.data.author)\($RESET)\n\($BODY_COLOR)📝 \(.data.selftext)\($RESET)\n─────────────────────────────"
 '
